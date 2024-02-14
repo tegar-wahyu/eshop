09b2019897a1da5b574dd9908f2d50fd25789299
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-02-14 18:21:41 +0700

    issue: remove unnecessary public modifier

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceImplTest {
+class ProductServiceImplTest {
     @Mock
     ProductRepository productRepository;
 
