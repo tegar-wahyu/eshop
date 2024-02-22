cac9be4eec870be681f245c0c9baca4441782c93
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-02-22 13:35:36 +0700

    feat: add Car Service and Impl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -0,0 +1,42 @@
+package id.ac.ui.cs.advprog.eshop.service;
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Autowired
+    private CarRepository carRepository;
+    @Override
+    public Car create(Car car) {
+        // TODO: Auto-generated method stub
+        carRepository.create(car);
+        return car;
+    }
+    @Override
+    public List<Car> findAll() {
+        Iterator<Car> carIterator = carRepository.findAll();
+        List<Car> allCar = new ArrayList<>();
+        carIterator.forEachRemaining(allCar::add);
+        return allCar;
+    }
+    @Override
+    public Car findById(String id) {
+        Car car = carRepository.findById(id);
+        return car;
+    }
+    @Override
+    public void update(String carId, Car car) {
+        // TODO: Auto-generated method stub
+        carRepository.update(carId, car);
+    }
+    @Override
+    public void deleteCarById(String id) {
+        // TODO: Auto-generated method stub
+        carRepository.delete(id);
+    }
+}
