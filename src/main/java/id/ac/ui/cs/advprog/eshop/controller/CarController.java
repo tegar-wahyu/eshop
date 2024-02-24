e96911f81766df21acfdd3152abf5702afc1d9c5
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-02-24 16:38:44 +0700

    refactor: implement DIP
    change dependency of CarController to the CarService interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,7 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +14,7 @@ import java.util.List;
 public class CarController {
 
     @Autowired
-    private CarServiceImpl carService;
+    private CarService carService;
 
     @GetMapping("/createCar")
     public String createCarPage(Model model){
