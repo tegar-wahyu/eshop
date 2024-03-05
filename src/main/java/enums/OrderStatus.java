7d012fa4e77db1063e80a8ebf4109252f4ccd150
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-03-05 22:58:56 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/enums/OrderStatus.java
@@ -0,0 +1,22 @@
+package enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELLED("CANCELLED");
+
+    private final String value;
+    private OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String value) {
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.value.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
