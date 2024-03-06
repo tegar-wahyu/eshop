938cea0a2670c5df134bbd8d04522e38981f4668
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-03-06 20:08:13 +0700

    [RED] Add PaymentBankTransfer skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+class PaymentBankTransfer extends Payment {
+    public PaymentBankTransfer(String id, String method, Map<String, String> paymentData) {
+        super(id, method, paymentData);
+    }
+
+    public PaymentBankTransfer(String id, String method, Map<String, String> paymentData, String status) {
+        super(id, method, paymentData, status);
+    }
+
+    @Override
+    public void setPaymentData(Map<String, String> paymentData) {}
+}
