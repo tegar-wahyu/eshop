715eabb3ff69f952f7aaf1240afc87a0a5e57ed3
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-03-06 17:55:10 +0700

    [RED] Add PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList<>();
+    public Payment save(Payment payment) {return null;}
+    public Payment findById(String id) {return null;}
+    public List<Payment> findAll() {return null;}
+}
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
